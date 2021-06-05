aa3ad7467793cbca6705f953a8aa5d2749d5fc5c
John Finberg <john@Johns-MacBook-Pro.local>
2021-06-04 21:33:12 -0700

    added more comments

--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,26 @@ from flask import Flask, request
 import z3_methods
 import json
 
+# flask setups
 app = Flask(__name__)
 z3 = z3_methods.Z3_Worker()
 
+# base page
 @app.route('/')
 def blank():
     return 'Project Orca Z3 API'
 
-@app.route('/solver', methods=['POST'])
+# handles proof checker
+@app.route('/checker', methods=['POST'])
 def request_handler():
+    # proof type
     data = json.loads(request.data)
     type = data['type']
     
+    # valid proof types
     valid_operations = {'algebraic': z3.algebraic, 'inequality': z3.inequality}
     if type in valid_operations:
-        return '{"valid: "'+str(valid_operations[type](data['code']))+'}'
+        # if valid proof type executes proof checker
+        return json.dump('{"success": "true", "valid: "'+str(valid_operations[type](data['code']))+'}')
     else:
-        return 'Error: Type not found.'
+        return json.dump('{"success": "false", "valid: "invalid"}')
