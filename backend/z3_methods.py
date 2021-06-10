20188233e0bc4b44b15eeca23150de8dbb68045d
mattgusdorff <mjgusdorff27@gmail.com>
2021-06-10 15:24:31 -0400

    please god work

--- backend/z3_methods.py
+++ backend/z3_methods.py
@@ -11,6 +11,13 @@ class Z3_Worker():
 
         return varibles
 
+    def char_in_str(self, input, list):
+        for char in list:
+            if char in input:
+                return char
+
+        return False
+
     # split code line by line
     def code_to_list(self, code):
         return code.split(',')
@@ -23,67 +30,6 @@ class Z3_Worker():
     # is this a definition
     def is_definition(self, code):
         return '=' in code
-    
-    #returns true if string contains a letter (caps or no caps)
-    def has_letters(self, string):
-        letters = [chr(i) for i in range (65,91)]
-        letters.extend([chr(i) for i in range(97, 123)])
-        for letter in letters:
-            if letter in string:
-                return True
-        
-        return False
-    
-    #returns True if there is no operator and there is a letter variable on the left
-    #of the inequality. No inequality returns False
-    def is_bounding(self, expression):
-        if '>' in expression:
-            index = expression.index('>')
-        elif '<' in expression:
-            index = expression.index('<')
-        else:
-            return False
-
-        for op in ['+', '-', '/', '*']:
-            if op in expression[0:index]:
-                return False
-        
-        if self.has_letters(expression[0:index]):
-            return True
-        else:
-            return False
-    
-    #returns a tuple of ([free vars], [bound vars])
-    def separate_vars(self, expressions):
-        vars = set()
-        bound = set()
-        for expression in expressions:
-            for var in self.info_on_expression(expression):
-                vars.add(var)
-                if self.is_bounding(expression):
-                    bound.add(var)
-        
-        vars = list(vars)
-        for var in vars:
-            if var in bound:
-                vars.remove(var)
-        
-        return (vars, list(bound))
-                
-        
-        
-    #returns a tuple of ([free expressions], [bound expressions])
-    def separate_expressions(self, expressions):
-        free = []
-        bound = []
-
-        for expression in expressions:
-            if self.is_bounding(expression):
-                bound.append(expression)
-            else:
-                free.append(expression)
-        
-        return (free, bound)
 
     # solve the proof
     def algebraic_solver(self, expressions):
@@ -118,12 +64,6 @@ class Z3_Worker():
     def inequality_solver(self, code):
         # find each line of code
         code_steps = self.code_to_list(code)
-        expression_list = []
-
-        # clean each line of code to an expression
-        for expression in code_steps:
-            if self.is_definition(expression):
-                expression_list.append(expression)
 
         s = Solver()
 
@@ -131,13 +71,16 @@ class Z3_Worker():
         declared_vars = []
 
         # declare new varibles and add statements to solver
-        for expression in expression_list:
+        for expression in code_steps:
             varibles = self.info_on_expression(expression)
 
             for var in [x for x in varibles if x not in declared_vars]:
+                declared_vars.append(var)
                 exec(var + " = Real('"+var+"')")
 
-            expression = expression.replace('=', '==')
+            if expression.count('=') == 1:
+                expression = expression.replace('=', '==')
+
             f = eval(expression)
             s.add(f)
 
@@ -152,20 +95,134 @@ class Z3_Worker():
         return self.inequality_solver(code)
 
     def simplify_tool(self, expression):
-        varibles = self.info_on_expression(expression)
+        expression_list = []
 
-        for var in varibles:
-            exec(var + " = Real('"+var+"')")
+        modifier = self.char_in_str(expression, ['>', '<', '='])
+        final_modifier = ''
 
-        f = eval(expression)
+        if modifier != False:
+            if modifier in ['>', '<', '=']:
+                if self.char_in_str(expression, ['=']):
+                    final_modifier = modifier + '='
+                    expression_list = expression.split(modifier+'=')
+                else:
+                    final_modifier = modifier
+                    expression_list = expression.split(modifier)
+
+            # hold simplified expression
+            simplified_expressions = []
+
+            # keep track of declared varibles to not dupe
+            declared_vars = []
+
+            # declare new varibles and add statements to solver
+            # simplify each side and add it to a list
+            for index, expression in enumerate(expression_list):
+                varibles = self.info_on_expression(expression)
+                
+                for var in [x for x in varibles if x not in declared_vars]:
+                    declared_vars.append(var)
+                    exec(var + " = Real('"+var+"')")
+
+                f = eval(expression)
+                if type(f) == z3.z3.ArithRef:
+                    simplified = simplify(f)
+                else:
+                    simplified = f
+                
+                if type(simplified) == z3.z3.BoolRef:
+                    if simplified or not simplified:
+                        simplified_expressions.append(expression_list[index])
+                else:
+                    simplified_expressions.append(simplified)
+
+            # simplify the combined statements
+            combined_expression = eval(final_modifier.join([str(elem) for elem in simplified_expressions]))
+            if combined_expression == True:
+                return final_modifier.join([str(elem) for elem in simplified_expressions])
+
+            final_expression = simplify(combined_expression)
+
+            if str(final_expression) == 'True' or str(final_expression) == 'False':
+                if final_expression or not final_expression:
+                    return str(combined_expression)
+            else:
+                return final_expression
+        else:
+            # keep track of declared varibles to not dupe
+            declared_vars = []
+
+            varibles = self.info_on_expression(expression)
+            for var in [x for x in varibles if x not in declared_vars]:
+                    exec(var + " = Real('"+var+"')")
+
+            return simplify(eval(expression))
+
+    def is_bounding(self, code):
+        varibles = self.info_on_expression(code)
+
+        matches_found = {'>': 0, '<': 0, '=': 0}
+        for char in code:
+            for special in ['<', '>', '=']:
+                if special == char:
+                    matches_found[char] += 1
         
-        return simplify(f)
+        if ((matches_found['>'] == 1 and matches_found['<'] == 0) or (matches_found['>'] == 0 and matches_found['<'] == 1) or (matches_found['='] == 2 and matches_found['<'] == 0 and matches_found['>'] == 0)) and len(self.info_on_expression(code)) == 1:
+            return True
+        
+        return False
 
+    def for_all(self, code):
+        # find each line of code
+        code_steps = self.code_to_list(code)
+
+        s = Solver()
+
+        # keep track of declared varibles to not dupe
+        declared_vars = []
+        bounding_vars = []
+        expression_list = []
+
+        # declare new varibles and add statements to solver
+        for expression in code_steps:
+            varibles = self.info_on_expression(expression)
+            
+            for var in [x for x in varibles if x not in declared_vars]:
+                    declared_vars.append(var)
+                    exec(var + " = Real('"+var+"')")
+
+            if self.is_bounding(expression):
+                for var in varibles:
+                    bounding_vars.append(var)
+                
+            expression_list.append(expression)
+
+        clean_vars = []
+        exec_expression_list = []
+        for expression in expression_list:
+            varibles = self.info_on_expression(expression)
+            expression = eval(expression)
+            exec_expression_list.append(expression)
+            for_all_vars = [x for x in varibles if x not in bounding_vars]
+            for var in for_all_vars:
+                clean_vars.append(locals()[for_all_vars[0]])
+
+            # print(str(clean_vars), str(expression))
+        
+        print(exec_expression_list)
+        print(clean_vars)
+        s.add(ForAll(clean_vars, exec_expression_list))
+
+        if s.check() == sat:
+            expression_model = s.model()
+        else:
+            expression_model = None
+        
+        return (s.check() == sat, expression_model)
+                
 if __name__ == '__main__':
     import json
     test = Z3_Worker()
-    print(test.algebraic(json.loads('{"type": "algebraic", "code": "x=2*3+4-2,x=6+4-4,x=10-2,x=8"}')['code']))
-    print(test.inequality("x=2,y=5,z=x+y,z>3"))
-    testExprs = ["x>1","x**2+y**2>1"]
-    print("Free:",test.separate_vars(testExprs)[0],"Bound:",test.separate_vars(testExprs)[1])
-    print("Free:",test.separate_expressions(testExprs)[0],"Bound:",test.separate_expressions(testExprs)[1])
+    # print(test.algebraic(json.loads('{"type": "algebraic", "code": "x=2*3+4-2,x=6+4-4,x=10-2,x=8"}')['code']))
+    # print(test.simplify_tool("2*x**2 +4*x**2"))
+    print(test.for_all("x>1, x**2+y**2>1"))
