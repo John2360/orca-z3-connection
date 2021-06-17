1995f2737136ddea9b99e62ac9219566ce300203
mattgusdorff <mjgusdorff27@gmail.com>
2021-06-17 18:58:13 -0400

    new site

--- frontend/index.js
+++ frontend/index.js
@@ -10,22 +10,7 @@ $(document).ready(() =>
         $.ajax({
             type : 'POST',
             contentType: "application/json",
-            url : 'http://127.0.0.1:5000/checker',
-            data : getContent(),
-            success : (data)  => 
-            {
-                console.log(data)
-            }
-        });
-    });
-
-    $("#simplify-button").on('click', () =>
-    {
-        console.log(getContent())
-        $.ajax({
-            type : 'POST',
-            contentType: "application/json",
-            url : 'http://127.0.0.1:5000/simplify',
+            url : 'http://blum.cs.haverford.edu:8080/',
             data : getContent(),
             success : (data)  => 
             {
