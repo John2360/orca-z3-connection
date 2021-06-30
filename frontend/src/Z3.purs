7a042804c84953d6e4beea87daa02519c5323406
mattgusdorff <mjgusdorff27@gmail.com>
2021-06-30 16:41:36 -0400

    progress maybe

--- frontend/src/Z3.purs
+++ frontend/src/Z3.purs
@@ -20,9 +20,10 @@ type API_Requests = {
     body :: String
 }
 
+--sample = {url:"http://blum.cs.haverford.edu:8080/checker", body:"{'expressions':'1'}"}
 sendRequest :: API_Requests -> Effect Unit --probably not the right types
 sendRequest info = launchAff_ do 
-    result2 <- AX.post ResponseFormat.json info.url (Just (RequestBody.json (fromString info.url)))
+    result2 <- AX.post ResponseFormat.json info.url (Just (RequestBody.json (fromString info.body)))
     case result2 of
         Left err -> log $ "POST " <> info.url <> "; response failed to decode: " <> AX.printError err
         Right response -> log $ "POST " <> info.url <> "; response: " <> stringify response.body
