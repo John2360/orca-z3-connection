4a10cdbb0e5e6ae9cc43bf0205d51166660f154e
mattgusdorff <mjgusdorff27@gmail.com>
2021-06-30 20:43:41 -0400

    please work

--- frontend/src/Z3.purs
+++ frontend/src/Z3.purs
@@ -9,6 +9,8 @@ import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Core (stringify, fromString, Json)
 import Effect.Class.Console (log)
 
+import Data.HTTP.Method (Method(..))
+import Affjax.RequestHeader
 import Effect (Effect)
 import Effect.Aff (Aff, launchAff_)
 import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
@@ -20,10 +22,11 @@ type API_Requests = {
     body :: String
 }
 
---sample = {url:"http://blum.cs.haverford.edu:8080/checker", body:"{'expressions':'1'}"}
+--sample = {url:"http://blum.cs.haverford.edu:8080/checker", body:"{'expressions':'x>2', 'bounds',''}"}
 sendRequest :: API_Requests -> Effect Unit --probably not the right types
 sendRequest info = launchAff_ do 
-    result2 <- AX.post ResponseFormat.json info.url (Just (RequestBody.json (fromString info.body)))
-    case result2 of
-        Left err -> log $ "POST " <> info.url <> "; response failed to decode: " <> AX.printError err
-        Right response -> log $ "POST " <> info.url <> "; response: " <> stringify response.body
+    result <- AX.request (AX.defaultRequest { url = "http://blum.cs.haverford.edu:8080/checker", method = Left POST, responseFormat = ResponseFormat.json, headers = [RequestHeader "Content-Type" "application/json"], content = (Just(RequestBody.json (fromString "{'expressions':'x>2'}")))})
+    case result of
+        Left err -> log $ "POST http://blum.cs.haverford.edu:8080/checker response failed to decode: " <> AX.printError err
+        Right response -> log $ "POST http://blum.cs.haverford.edu:8080/checker response: " <> stringify response.body
+        
